5e5ee4679ab6727d5a073b6e198dda8e9213424f
vishalBhangare <vishalbhangare2@gmail.com>
2023-11-28 21:35:07 +0530

    patch 0.1(auth)

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,12 +12,12 @@ export default async function createSupabaseServerClient() {
         get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        // set(name: string, value: string, options: CookieOptions) {
-        //   cookieStore.set({ name, value, ...options });
-        // },
-        // remove(name: string, options: CookieOptions) {
-        //   cookieStore.set({ name, value: "", ...options });
-        // },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options });
+        },
       },
     }
   );
